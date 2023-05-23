cf1512b3e1eabd39e00b2a084dca39d66022d1d8
Akshatchourey <achourey09@gmail.com>
2023-05-23 22:27:16 +0530

    cleaning tree, to test some remote commit to private repo what hapnes

--- other/rough.py
+++ other/rough.py
@@ -60,7 +60,36 @@ print(d)
 '''
 
 
-    
+
+
+# Merge the Tools! Question HackerRank:-
+"""
+def merge_the_tools(s, k):
+    arr = []
+    n = len(s) / k
+    n = int(n)
+    a = 0
+    b = k
+    for i in range(n):
+        arr.append(s[a:b])
+        a = b
+        b += k
+
+    for i in range(n):
+        s1 = arr[i]
+        s2 = ""
+        for k in s1:
+            if k in s2:
+                continue
+            s2 += k
+        print(s2)
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
+"""
+
 
 
 
